d14bce661197bea418f8c8f35a715bbbc9a01dac
SungHoon Im <shi510.kor@gmail.com>
2019-04-05 11:48:20 +0900

    do not load entire images when training starts. instead of that, load batch images per iteration. TODO: loading using thread.

--- Image_Generator.py
+++ Image_Generator.py
@@ -22,31 +22,26 @@ class TextImageGenerator:
         self.img_dirpath = img_dirpath                  # image dir path
         self.img_dir = os.listdir(self.img_dirpath)     # images list
         self.n = len(self.img_dir)                      # number of images
-        self.indexes = list(range(self.n))
         self.cur_index = 0
-        self.imgs = np.zeros((self.n, self.img_h, self.img_w))
-        self.texts = []
+        self.last_load_idx = 0
 
-    ## samples의 이미지 목록들을 opencv로 읽어 저장하기, texts에는 label 저장
-    def build_data(self):
-        print(self.n, " Image Loading start...")
-        for i, img_file in enumerate(self.img_dir):
-            img = cv2.imread(self.img_dirpath + img_file, cv2.IMREAD_GRAYSCALE)
+    def next_sample(self):
+        batch_img = []
+        batch_label = []
+        for n in range(self.batch_size):
+            idx = self.last_load_idx + n
+            if idx >= self.n:
+                self.last_load_idx = -n
+                idx = 0
+                random.shuffle(self.img_dir)
+            img = cv2.imread(self.img_dirpath + self.img_dir[idx], cv2.IMREAD_GRAYSCALE)
             img = cv2.resize(img, (self.img_w, self.img_h))
             img = img.astype(np.float32)
             img = (img / 255.0) * 2.0 - 1.0
-
-            self.imgs[i, :, :] = img
-            self.texts.append(img_file[0:-4])
-        print(len(self.texts) == self.n)
-        print(self.n, " Image Loading finish...")
-
-    def next_sample(self):      ## index max -> 0 으로 만들기
-        self.cur_index += 1
-        if self.cur_index >= self.n:
-            self.cur_index = 0
-            random.shuffle(self.indexes)
-        return self.imgs[self.indexes[self.cur_index]], self.texts[self.indexes[self.cur_index]]
+            batch_img.append(img)
+            batch_label.append(self.img_dir[idx][0:-4])
+        self.last_load_idx += self.batch_size
+        return batch_img, batch_label
 
     def next_batch(self):       ## batch size만큼 가져오기
         while True:
@@ -54,15 +49,13 @@ class TextImageGenerator:
             Y_data = np.ones([self.batch_size, self.max_text_len])             # (bs, 9)
             input_length = np.ones((self.batch_size, 1)) * (self.img_w // self.downsample_factor - 2)  # (bs, 1)
             label_length = np.zeros((self.batch_size, 1))           # (bs, 1)
-
+            img, text = self.next_sample()
             for i in range(self.batch_size):
-                img, text = self.next_sample()
-                img = img.T
-                img = np.expand_dims(img, -1)
-                X_data[i] = img
-                Y_data[i] = text_to_labels(text)
-                label_length[i] = len(text)
-
+                img[i] = img[i].T
+                img[i] = np.expand_dims(img[i], -1)
+                X_data[i] = img[i]
+                Y_data[i] = text_to_labels(text[i])
+                label_length[i] = len(text[i])
             # dict 형태로 복사
             inputs = {
                 'the_input': X_data,  # (bs, 128, 64, 1)
